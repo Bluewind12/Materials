1f7c27cf2fa60a8fc455be7b8d94fa29bc9a7381
BlueWind <zero18030@gmail.com>
2021-01-20 02:06:29 +0900

    [add]DB上にデータを入力できるように修正 - ゲーム名のDB作成

---
+++ database/migrations/2021_01_19_165426_create_game_names_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGameNamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('game_names', function (Blueprint $table) {
+            $table->bigIncrements('id')->comment("ID");
+            $table->string('name')->comment("ゲーム名");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('game_names');
+    }
+}
