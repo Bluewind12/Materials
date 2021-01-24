b48d7638995f40fc40984bd0f1adcbf127818f29
“BlueWind_Mac” <zero18030@gmail.com>
2021-01-24 21:36:24 +0900

    #15 画面構成の修正

--- resources/views/common/side_bar.blade.php
+++ resources/views/common/side_bar.blade.php
@@ -1,14 +1,14 @@
 <div class="side-bar d-flex flex-column align-items-center
     px-2 py-4 border-top border-bottom border-white"
-     style="width: 150px;">
-    <div class="h4 px-2 border-white border-bottom">@fa('list')　一覧</div>
-    <a href="{{route('list.stages')}}" class="text-reset">ステージ</a>
-    <a href="{{route('list.materials')}}" class="text-reset">素材</a>
+     style="width: 200px;">
+    <div class="h4 w-100 px-2 border-white border-bottom text-center">@fa('map')　ステージ</div>
+    <a href="{{route('stages.view')}}" class="text-reset">・一覧</a>
+    <a href="{{route('stages.store')}}" class="text-reset">・登録</a>
 
-    <div class="h4 px-2 mt-4 border-white border-bottom">@fa('pen')　登録</div>
-    <a href="{{route('add.stages')}}" class="text-reset">ステージ登録</a>
-    <a href="{{route('add.materials')}}" class="text-reset">素材登録</a>
+    <div class="h4 w-100 px-2 mt-4 border-white border-bottom text-center">@fa('gem')　　素材　</div>
+    <a href="{{route('materials.view')}}" class="text-reset">・一覧</a>
+    <a href="{{route('materials.store')}}" class="text-reset">・登録</a>
 
-    <div class="h4 px-2 mt-4 border-white border-bottom">@fa('calculator')　計算</div>
-    <a href="#" class="text-reset">周回計算</a>
+    <div class="h4 w-100 px-2 mt-4 border-white border-bottom text-center">@fa('calculator')　　計算　</div>
+    <a href="#" class="text-reset">・周回計算</a>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -19,19 +19,25 @@ Route::get('/test', function () {
     return view('test');
 });
 
-Route::get('/list/materials', function () {
-    return view('list/material_list');
-})->name('list.materials');
 
-Route::get('/list/stages', function () {
-    return view('list/stage_list');
-})->name('list.stages');
+Route::name('materials.')->group(function (){
+    Route::get('/materials/list',function (){
+        return view('materials/list');
+    })->name('view');
 
-Route::get('/add/materials', function () {
-    return view('add/material_add');
-})->name('add.materials');
-Route::get('/add/stages', function () {
-    return view('add/stage_add');
-})->name('add.stages');
+    Route::get('/materials/store',function (){
+        return view('materials/add');
+    })->name('store');
+});
+
+Route::name('stages.')->group(function (){
+    Route::get('/stages/list',function (){
+        return view('stages/list');
+    })->name('view');
+
+    Route::get('/stages/store',function (){
+        return view('stages/add');
+    })->name('store');
+});
 
 
